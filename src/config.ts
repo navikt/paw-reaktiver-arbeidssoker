8b6028d672cc5c4e697922859f5d2ba60df1281f
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-12-07 13:45:43 +0100

    legger til kafka ssl config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 export interface IEnvironmentVariables {
     KAFKA_TOPIC: string;
     KAFKA_BROKERS: string;
+    KAFKA_CERTIFICATE: string;
+    KAFKA_CA: string;
+    KAFKA_PRIVATE_KEY: string;
     AIA_BACKEND_URL: string;
     VEILARBREGISTRERING_URL: string;
     VEILARBREGISTRERING_GCP_URL: string;
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import config from './config';
 const kafka = new Kafka({
     clientId: 'paw-reaktiver-arbeidssoker',
     brokers: [config.KAFKA_BROKERS],
+    ssl: {
+        rejectUnauthorized: false,
+        ca: [config.KAFKA_CA],
+        key: config.KAFKA_PRIVATE_KEY,
+        cert: config.KAFKA_CERTIFICATE,
+    },
 });
 
 const consumer = kafka.consumer({ groupId: 'test-group-v1' });
