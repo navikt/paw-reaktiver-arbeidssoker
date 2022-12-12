5c8cc086695bd7012c24b4d5372fcbde3e34d645
Geir Gåsodden <geir.gasodden@gmail.com>
2022-12-12 11:02:48 +0100

    r legger til flere test-scenarier

--- src/lib/skal-melding-behandles.test.ts
+++ src/lib/skal-melding-behandles.test.ts
@@ -1,22 +1,60 @@
-import test from 'node:test';
+import { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
 
 import { skalMeldingBehandles } from './skal-melding-behandles';
+import { plussDager } from './plussdager';
 
 import { Melding } from '../types/melding';
 
-test('returnerer false dersom arbeidssøkeren har svar NEI på spørsmål 5', () => {
-    const melding = {
-        fnr: '12345678910',
-        arbeidssokerNestePeriode: false,
-        periodeFra: '2022-11-28',
-        periodeTil: '2022-12-11',
-        kortType: 'ELEKTRONISK',
-        opprettet: '2022-12-12',
-    } as Melding;
+describe('tester skalMeldingBehandles', () => {
+    it('returnerer false dersom arbeidssøkeren har svart NEI på spørsmål 5', () => {
+        const melding = {
+            fnr: '12345678910',
+            arbeidssokerNestePeriode: false,
+            periodeFra: '2022-11-28',
+            periodeTil: '2022-12-11',
+            kortType: 'ELEKTRONISK',
+            opprettet: '2022-12-12',
+        } as Melding;
 
-    const resultat = skalMeldingBehandles(melding);
-    const forventetResultat = false;
+        const resultat = skalMeldingBehandles(melding);
+        const forventetResultat = false;
 
-    assert.equal(resultat, forventetResultat);
+        assert.equal(resultat, forventetResultat);
+    });
+
+    it('returnerer false dersom arbeidssøkeren har svart JA på spørsmål 5 og periodeTil + 14 dager er i fortiden', () => {
+        const melding = {
+            fnr: '12345678910',
+            arbeidssokerNestePeriode: true,
+            periodeFra: '2021-11-28',
+            periodeTil: '2021-12-11',
+            kortType: 'ELEKTRONISK',
+            opprettet: '2021-12-12',
+        } as Melding;
+
+        const resultat = skalMeldingBehandles(melding);
+        const forventetResultat = false;
+
+        assert.equal(resultat, forventetResultat);
+    });
+
+    it('returnerer true dersom arbeidssøkeren har svart JA på spørsmål 5 og periodeTil + 14 dager er i fremtiden', () => {
+        const iDag = new Date();
+        const iDagStreng = iDag.toISOString().substring(0, 10);
+        const periodeFra = plussDager(iDag, -7).toISOString().substring(0, 10);
+        const melding = {
+            fnr: '12345678910',
+            arbeidssokerNestePeriode: true,
+            periodeFra,
+            periodeTil: iDagStreng,
+            kortType: 'ELEKTRONISK',
+            opprettet: iDagStreng,
+        } as Melding;
+
+        const resultat = skalMeldingBehandles(melding);
+        const forventetResultat = true;
+
+        assert.equal(resultat, forventetResultat);
+    });
 });
