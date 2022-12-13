0fa201c8c57214ee39e10cf6402ec7ce959efebb
Geir Gåsodden <geir.gasodden@gmail.com>
2022-12-13 07:21:59 +0100

    d beskriver flyt for løsning i kommentar

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Kafka } from 'kafkajs';
 
 import config from './config';
 import logger from './logger';
+
 import { skalMeldingBehandles } from './lib/skal-melding-behandles';
 
 import { Melding } from './types/melding';
@@ -33,18 +34,27 @@ const consumer = kafka.consumer({ groupId: `${config.APP_NAME}-group-v1` });
     await consumer.run({
         eachMessage: async ({ message }) => {
             if (message.value) {
-                const { value } = message;
+                const { value, offset } = message;
                 try {
                     const messageJSON = JSON.parse(value.toString()) as Melding;
                     const skalBehandles = skalMeldingBehandles(messageJSON);
                     if (skalBehandles) {
-                        logger.info('Behandler meldingen');
+                        logger.info(`Behandler meldingen med offset - ${offset}`);
+                        /**
+                         * - Sjekker om arbeidssøkeren ikke har aktiv arbeidssøkerperiode
+                         *      - henter arbeidssøkerperioder og kjører resultatet gjennom kan-arbeidsøkeren-reaktiveres
+                         * - Hvis ikke reaktivering er aktuelt: Avbryt
+                         * - Hvis reaktivering er aktuelt:
+                         *      - reaktiver arbeidssøker
+                         *      - hvis reaktivering feiler: avbryt
+                         *      - hvis reaktivering suksess
+                         *          - post melding om reaktivering til AiA-backend
+                         */
                     } else {
-                        logger.info('Meldingen skal ikke behandles');
+                        logger.info(`Meldingen med offset - ${offset} - skal ikke behandles`);
                     }
                 } catch (error) {
-                    logger.error(`Feil ved lesing av kafka melding: ${message}`);
-                    logger.error(error);
+                    logger.error(`Feil ved lesing av kafka melding: ${error}`);
                 }
             }
         },
