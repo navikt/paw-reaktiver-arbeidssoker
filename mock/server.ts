a2e5ed73c9f44dc34942553080ff45e08d9c78a7
Geir Gåsodden <geir.gasodden@gmail.com>
2022-12-14 08:35:46 +0100

    r oppdaterer mock server med flere korrekte endepunkt og responser

--- mock/server.ts
+++ mock/server.ts
@@ -1,19 +1,29 @@
 import http from 'node:http';
+import { randomUUID } from 'node:crypto';
 
 http.createServer((req, res) => {
     const path = req.url;
     console.log(path);
-    if (path === '/arbeidssoker') {
+    if (path === '/api/arbeidssoker/perioder') {
+        const data = {
+            arbeidssokerperioder: [],
+        };
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end('{hello: world}');
-    } else if (path === '/fullfoerreaktivering') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end('{hello: world}');
-    } else if (path === '/lagre-data') {
+        res.end(JSON.stringify(data));
+    } else if (path === '/api/fullfoerreaktivering/systembruker') {
         res.writeHead(204);
         res.end();
+    } else if (path === '/m2m/automatisk-reaktivering') {
+        res.writeHead(201);
+        res.end();
+    } else if (path === '/token') {
+        const data = {
+            access_token: randomUUID(),
+        };
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(data));
     } else {
-        res.write('hello\n');
+        res.writeHead(204);
         res.end();
     }
 }).listen(3000);
