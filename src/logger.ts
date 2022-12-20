f1b9618f75f70ffff0dd04ba6363bd587e010c7b
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-12-20 09:06:10 +0100

    r legger til @timestamp logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,13 @@
 import pino from 'pino';
 
 export default pino({
+    timestamp: () => `,"@timestamp":"${new Date().toISOString()}"`,
     formatters: {
         level: (label: string) => ({ level: label }),
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         log: (object: any) => {
             object['x_callId'] = object.callId;
+            delete object.timestamp;
             if (object.err) {
                 const err = object.err instanceof Error ? pino.stdSerializers.err(object.err) : object.err;
                 object.stack_trace = err.stack;
