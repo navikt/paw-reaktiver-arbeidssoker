5a5bb616ed10d197fb25047af7c3d9b7cdc9ccb7
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-12-21 09:48:38 +0100

    r fjerner dp-innsyn fra outbound rules

--- src/lib/skal-melding-behandles.test.ts
+++ src/lib/skal-melding-behandles.test.ts
@@ -8,14 +8,14 @@ import { MeldekortMelding } from '../types/meldekort-melding';
 
 describe('tester skalMeldingBehandles', () => {
     it('returnerer FALSE dersom arbeidssøkeren har svart NEI på spørsmål 5', () => {
-        const melding = {
+        const melding: MeldekortMelding = {
             fnr: '12345678910',
             arbeidssokerNestePeriode: false,
             periodeFra: '2022-11-28',
             periodeTil: '2022-12-11',
             kortType: 'ELEKTRONISK',
             opprettet: '2022-12-12',
-        } as MeldekortMelding;
+        };
 
         const resultat = skalMeldingBehandles(melding);
         const forventetResultat = false;
@@ -24,14 +24,14 @@ describe('tester skalMeldingBehandles', () => {
     });
 
     it('returnerer FALSE dersom arbeidssøkeren har svart JA på spørsmål 5 og periodeTil + 14 dager er i FORTIDEN', () => {
-        const melding = {
+        const melding: MeldekortMelding = {
             fnr: '12345678910',
             arbeidssokerNestePeriode: true,
             periodeFra: '2021-11-28',
             periodeTil: '2021-12-11',
             kortType: 'ELEKTRONISK',
             opprettet: '2021-12-12',
-        } as MeldekortMelding;
+        };
 
         const resultat = skalMeldingBehandles(melding);
         const forventetResultat = false;
@@ -43,14 +43,14 @@ describe('tester skalMeldingBehandles', () => {
         const iDag = new Date();
         const iDagStreng = iDag.toISOString().substring(0, 10);
         const periodeFra = plussDager(iDag, -7).toISOString().substring(0, 10);
-        const melding = {
+        const melding: MeldekortMelding = {
             fnr: '12345678910',
             arbeidssokerNestePeriode: true,
             periodeFra,
             periodeTil: iDagStreng,
             kortType: 'ELEKTRONISK',
             opprettet: iDagStreng,
-        } as MeldekortMelding;
+        };
 
         const resultat = skalMeldingBehandles(melding);
         const forventetResultat = true;
