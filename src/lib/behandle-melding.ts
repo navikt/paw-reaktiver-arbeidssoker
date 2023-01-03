de6ea7c85a20dd1c7e3066f0de1e096ef42e047d
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-01-03 10:54:14 +0100

    r fjerner ubrukt kode

--- src/lib/behandle-melding.ts
+++ src/lib/behandle-melding.ts
@@ -1,8 +1,6 @@
 import logger from '../logger';
 
 import skalMeldingBehandles from './skal-melding-behandles';
-// import hentArbeidssokerperioder from './hent-arbeidssokerperioder';
-// import kanArbeidssokerenReaktiveres from './kan-arbeidssokeren-reaktiveres';
 import reaktiverBruker from './reaktiver-bruker';
 import lagreReaktiveringForBruker from './lagre-reaktivering-for-bruker';
 import { MeldekortMelding } from '../types/meldekort-melding';
@@ -27,25 +25,6 @@ export default async function (meldekortMelding: MeldekortMelding, offset: strin
             logger.info({ callId, message: `Bruker kan ikke reaktiveres - ${offset}` });
             return;
         }
-        // logger.info({ callId, message: `Henter arbeidssokerperioder for bruker - offset ${offset}` });
-        // const { arbeidssokerperioder } = await hentArbeidssokerperioder(fnr);
-        // if (!arbeidssokerperioder || arbeidssokerperioder.length === 0) {
-        //     logger.info({ callId, message: `Ingen arbeidssokerperioder funnet - offset ${offset}` });
-        //     return;
-        // }
-
-        // logger.info({
-        //     callId,
-        //     message: `${arbeidssokerperioder.length} arbeidssokerperioder funnet - offset ${offset}`,
-        // });
-
-        // if (!kanArbeidssokerenReaktiveres(arbeidssokerperioder)) {
-        //     logger.info({
-        //         callId,
-        //         message: `Bruker kan ikke reaktiveres, enten fordi det finnes en aktiv arbeidsøkerperiode eller fordi det er over 28 dager siden siste arbeidssøkerperiode - offset ${offset}`,
-        //     });
-        //     return;
-        // }
 
         logger.info({ callId, message: `Forsøker å reaktivere bruker - offset ${offset}` });
         await reaktiverBruker(fnr);
--- src/lib/dager-fra-dato.ts
+++
@@ -1,8 +0,0 @@
-export default function dagerFraDato(fra: Date, til?: Date): number {
-    const todate = til || new Date();
-    const start = new Date(fra.toISOString().substring(0, 10));
-    const end = new Date(todate.toISOString().substring(0, 10));
-    const millis = end.getTime() - start.getTime();
-    const days = millis / 3600000 / 24;
-    return Math.floor(days);
-}
