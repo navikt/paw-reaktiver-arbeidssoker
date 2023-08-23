4b58dd22c66599c4d8898efc8bd30ff42707504d
Jostein Holje <jostein.holje@nav.no>
2023-08-23 15:21:20 +0200

    Mer debug logging

--- src/unleash.ts
+++ src/unleash.ts
@@ -4,21 +4,23 @@ import logger from './logger';
 
 class ByEnvironmentStrategy extends Strategy {
     isEnabled(parameters: { [key: string]: string }, context: Context): boolean {
+        logger.info(parameters, 'isEnabled strategi - parametere');
+        logger.info(context, 'isEnabled strategi - context');
         const environmentList: string[] = parameters['miljø']?.split(',') || [];
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         return environmentList.includes(context.environment!);
     }
 }
 
-const unleashInit = () => logger.info(`UNLEASH_SERVER_API_TOKEN ${config.UNLEASH_SERVER_API_TOKEN}`);
-startUnleash({
-    appName: 'paw-reaktiver-arbeidssoker',
-    url: config.UNLEASH_SERVER_API_URL,
-    customHeaders: {
-        Authorization: config.UNLEASH_SERVER_API_TOKEN,
-    },
-    strategies: [new ByEnvironmentStrategy('byEnvironment')],
-});
+const unleashInit = () =>
+    startUnleash({
+        appName: 'paw-reaktiver-arbeidssoker',
+        url: config.UNLEASH_SERVER_API_URL,
+        customHeaders: {
+            Authorization: config.UNLEASH_SERVER_API_TOKEN,
+        },
+        strategies: [new ByEnvironmentStrategy('byEnvironment')],
+    });
 
 enum FeatureToggles {
     HENT_NESTE_FRA_KO = 'paw-reaktiver-arbeidssoker.hent-neste-fra-ko',
@@ -26,7 +28,7 @@ enum FeatureToggles {
 
 const toggleIsEnabled = (toggle: FeatureToggles) => {
     if (config.isDev) return true;
-
+    logger.info(`isEnabled: ${toggle}`);
     return isEnabled(toggle);
 };
 
